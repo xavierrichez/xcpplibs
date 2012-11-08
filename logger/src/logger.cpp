ef28062dad50695d158635c5ae8ad30d918882af
xavier.richez <xavier.richez@gmail.com>
2012-11-08 14:06:02 +0000

    optimisations

--- logger/src/logger.cpp
+++ logger/src/logger.cpp
@@ -21,18 +21,21 @@ void xlog::setLogLevel(int l)
 
 void xlog::setLogLevel(const char *l)
 {
-	std::string strLevel = l;
-	if ((strLevel == "TRACE") || (strLevel == "Trace") || (strLevel == "trace"))
+	std::string strLevel(l);
+	for(int i=0; i<strLevel.size(); i++)
+		strLevel[i] = toupper(strLevel[i]);
+
+	if (strLevel == "TRACE")
 		xlog::level = xlog::level_TRACE;
-	else if ((strLevel == "DEBUG") || (strLevel == "Debug") || (strLevel == "debug"))
+	else if (strLevel == "DEBUG")
 		xlog::level = xlog::level_DEBUG;
-	else if ((strLevel == "INFO") || (strLevel == "Info") || (strLevel == "info"))
+	else if (strLevel == "INFO")
 		xlog::level = xlog::level_INFO;
-	else if ((strLevel == "WARN") || (strLevel == "Warn") || (strLevel == "warn"))
+	else if (strLevel == "WARN")
 		xlog::level = xlog::level_WARN;
-	else if ((strLevel == "ERROR") || (strLevel == "Error") || (strLevel == "error"))
+	else if (strLevel == "ERROR")
 		xlog::level = xlog::level_ERROR;
-	else if ((strLevel == "NO_LOG") || (strLevel == "No_log") || (strLevel == "no_log"))
+	else if (strLevel == "NO_LOG")
 		xlog::level = xlog::level_NO_LOG;
 }
 
@@ -51,6 +54,18 @@ const char *xlog::dateTime()
 	return dateTimeStr;
 }
 
+void formatMessage(std::string &result, const char *class_name, const char *operation, const char *message)
+{
+	if (xlog::printDate)
+		result.append(xlog::dateTime()).append(" ");
+	if (class_name)
+		result.append(class_name);
+	if (operation)
+		result.append("::").append(operation).append(" - ");
+	if (message)
+		result.append(message);
+}
+
 /// Error message.
 //===============================================
 void xlog::error(const char *class_name, const char *operation, const char *message)
@@ -61,20 +76,8 @@ void xlog::error(const char *class_name, const char *operation, const char *mess
 	else if (xlog::level <= level_ERROR)
 	{
 		std::string msg = "!!ERROR -->> ";
-		
-		if (printDate)
-			msg = msg + dateTime();
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;		
-			
-		if (! msg.empty())			
-		{
-			std::cout<<msg<<std::endl;
-		}
+		formatMessage(msg, class_name, operation, message);
+		std::cout<<msg<<std::endl;
 	}
 }
 
@@ -96,20 +99,8 @@ void xlog::warn(const char *class_name, const char *operation, const char *messa
 	else if (xlog::level <= level_WARN)
 	{
 		std::string msg = "- WARN - ";
-		
-		if (printDate)
-			msg = msg + dateTime();
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;		
-			
-		if (! msg.empty())			
-		{
-			std::cout<<msg<<std::endl;
-		}
+		formatMessage(msg, class_name, operation, message);
+		std::cout<<msg<<std::endl;
 	}
 }
 
@@ -130,20 +121,9 @@ void xlog::info(const char *class_name, const char *operation, const char *messa
 		msg("log", "info", "No value set for xlog::level !");
 	else if (xlog::level <= level_INFO)
 	{
-		std::string msg = " (INFO) ";
-		
-		if (printDate)
-			msg = msg + dateTime();
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;	
-		if (! msg.empty())			
-		{
-			std::cout<<msg<<std::endl;
-		}
+		std::string msg = "( INFO ) ";
+		formatMessage(msg, class_name, operation, message);
+		std::cout<<msg<<std::endl;
 	}
 }
 
@@ -165,19 +145,8 @@ void xlog::debug(const char *class_name, const char *operation, const char *mess
 	else if (xlog::level <= level_DEBUG)
 	{
 		std::string msg = "  DEBUG  ";
-
-		if (printDate)
-			msg = msg + dateTime();
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;	
-		if (! msg.empty())			
-		{
-			std::cout<<msg<<std::endl;
-		}
+		formatMessage(msg, class_name, operation, message);
+		std::cout<<msg<<std::endl;
 	}
 }
 
@@ -199,19 +168,8 @@ void xlog::trace(const char *class_name, const char *operation, const char *mess
 	else if (xlog::level <= level_TRACE)
 	{
 		std::string msg = "  TRACE  ";
-
-		if (printDate)
-			msg = msg + dateTime();
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;	
-		if (! msg.empty())			
-		{
-			std::cout<<msg<<std::endl;
-		}
+		formatMessage(msg, class_name, operation, message);
+		std::cout<<msg<<std::endl;
 	}
 }
 
@@ -229,16 +187,7 @@ void xlog::msg(const char *class_name, const char *operation, const char *messag
 //===============================================
 {
 	std::string msg;
-
-	if (printDate)
-		msg = msg + dateTime();
-	if (class_name)
-		msg = msg + class_name;
-	if (operation)
-		msg = msg + "::" + operation + "  ";
-	if (message)
-		msg = msg + message;
-		
+	formatMessage(msg, class_name, operation, message);
 	if (! msg.empty())
 	{
 		std::cout<<msg<<std::endl;
@@ -262,7 +211,7 @@ void xlog::syserr(const char *message)
 		std::string msg;
 		if (printDate)
 			msg.append(dateTime());
-		msg = msg + std::string("System error : ") + message;
+		msg.append("System error : ").append(message);
 		std::cerr<<msg<<std::endl;
 	}
 }
@@ -276,8 +225,8 @@ void xlog::fatal(const char *message)
 	{
 		std::string msg;
 		if (printDate)
-			msg = msg + dateTime();
-		msg = msg + std::string("Fatal error ! The program will exit. Message is :\n") + message;
+			msg.append(dateTime());
+		msg.append("Fatal error ! The program will exit. Message is :\n").append(message);
 		std::cerr<<msg<<std::endl;
 	}
 	exit(EXIT_FAILURE);
@@ -296,18 +245,13 @@ void xlog::fatal(const char *class_name, const char *operation, const char *mess
 	{
 		std::string msg;
 		if (printDate)
-			msg = msg + dateTime();
-		msg = msg + "Fatal error ! ";
-		if (class_name)
-			msg = msg + class_name;
-		if (operation)
-			msg = msg + "::" + operation + "  ";
-		if (message)
-			msg = msg + message;
-		if (! msg.empty())
 		{
-			std::cerr<<msg<<std::endl;
+			msg.append(dateTime());
+			xlog::printDate = false;
 		}
+		msg.append("Fatal error ! The program will exit. Message is :\n");
+		formatMessage(msg, class_name, operation, message);
+		std::cerr<<msg<<std::endl;
 	}
 	exit(EXIT_FAILURE);
 }
