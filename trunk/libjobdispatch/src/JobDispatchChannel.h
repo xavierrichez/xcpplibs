3cfffeb0d3064ce7e7c549ab922a0b650a6af0a6
xavier.richez@gmail.com <xavier.richez@gmail.com@d99a8e18-ae59-5acc-b674-b7fce174f5e2>
2013-07-03 12:41:48 +0000

    added GNU LGPL Header

--- trunk/libjobdispatch/src/JobDispatchChannel.h
+++ trunk/libjobdispatch/src/JobDispatchChannel.h
@@ -1,13 +1,29 @@
 #ifndef JOBDISPATCHCHANNEL_H_
 #define JOBDISPATCHCHANNEL_H_
 
+/**********************************************************************************
+ * JobDispatchChannel : this channel targets the services.
+ * The master sends the jobs on this channel.
+ *
+ **********************************************************************************
+ * Copyright (C) 2012 by Xavier Richez
+ * xavier[.]richez[@]laposte.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ **********************************************************************************/
+
 #include "MessageChannel.h"
 #include "MessageHandler.h"
 #include "JobInfo.hpp"
 
-/**
- * Canal de messages pour l'envoi de commandes par le master
- */
 class JobDispatchChannel : public MessageChannel<JobInfo> {
 public:
 	JobDispatchChannel(const char *channelName, MessageHandler<JobInfo> *handler=NULL);
