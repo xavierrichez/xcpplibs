71303f4b1f29e7ef4b0c302c454bff037e09696c
xavier.richez <xavier.richez@gmail.com>
2013-07-03 12:39:31 +0000

    added GNU LGPL Header

--- libjobdispatch/src/JobManager.h
+++ libjobdispatch/src/JobManager.h
@@ -1,6 +1,24 @@
 #ifndef JOBMANAGER_H_
 #define JOBMANAGER_H_
 
+/**********************************************************************************
+ * JobManager : abstract class used by the JobDispatcher to get the job content.
+ *
+ **********************************************************************************
+ * Copyright (C) 2012 by Xavier Richez
+ * xavier[.]richez[@]laposte.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ **********************************************************************************/
+
 #include <string>
 #include "JobInfo.hpp"
 
