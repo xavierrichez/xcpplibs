1f4dbe43f95f200b8eefd736a9f78ed566ed70de
xavier.richez@gmail.com <xavier.richez@gmail.com@d99a8e18-ae59-5acc-b674-b7fce174f5e2>
2013-07-03 12:37:18 +0000

    added GNU LGPL Header

--- trunk/libjobdispatch/src/MessageException.h
+++ trunk/libjobdispatch/src/MessageException.h
@@ -1,8 +1,25 @@
 #ifndef MESSAGEEXCEPTION_H_
 #define MESSAGEEXCEPTION_H_
 
+/**********************************************************************************
+ * MessageException : Thrown on communication error.
+ *//*******************************************************************************
+ * Copyright (C) 2012 by Xavier Richez
+ * xavier[.]richez[@]laposte.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ **********************************************************************************/
+
 #include <exception>
-#include "stringcollections.h"
+#include <string>
 
 class MessageException: public std::exception {
 public:
@@ -13,4 +30,4 @@ private:
    std::string message;
 };
 
-#endif /* PROCESSEXCEPTION_H_ */
+#endif /* MESSAGEEXCEPTION_H_ */
