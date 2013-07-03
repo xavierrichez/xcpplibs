ac126a3146acf5651059da70e3c7c95b604a9e51
xavier.richez <xavier.richez@gmail.com>
2013-07-03 12:40:19 +0000

    added GNU LGPL Header

--- libjobdispatch/src/JobHandler.h
+++ libjobdispatch/src/JobHandler.h
@@ -1,6 +1,25 @@
 #ifndef JOBHANDLER_H_
 #define JOBHANDLER_H_
 
+/**********************************************************************************
+ * JobHandler : client service prototype.
+ * A service implementation only has to extend this class and to implement the handle(job) function.
+ *
+ **********************************************************************************
+ * Copyright (C) 2012 by Xavier Richez
+ * xavier[.]richez[@]laposte.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ **********************************************************************************/
+
 #include <string>
 #include "MessageHandler.h"
 #include "JobInfo.hpp"
